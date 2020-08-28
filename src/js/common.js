4065954659020c982af708ed11f2e0e424be3e38
Igor Makarchuk <ingvardellyn@gmail.com>
2020-08-28 19:53:16 +0300

    commit pre-test w3c is clean

--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,7 @@ const menuItems = document.querySelector(".menu-items");
 const menuBg = document.querySelector(".menu-bg");
 const anchors = document.querySelectorAll("a.scrl-m");
 const body = document.querySelector("body");
+const lay = document.getElementById("lay");
 for (let anchor of anchors) {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -21,18 +22,21 @@ for (let anchor of anchors) {
 }
 
 // Menu for mobile
-
 function toggleMenu() {
-  // e.preventDefault;
   menu.classList.toggle("fs");
   menuBg.classList.toggle("fs");
   menuItems.classList.toggle("fs");
   menu.textContent == "☰" ? (menu.textContent = "✕") : (menu.textContent = "☰");
+  lay.classList.remove("dn");
+
   if (menu.classList.contains("fs")) {
     body.classList.add("overflow-h");
   } else {
     body.classList.remove("overflow-h");
   }
 }
-
 menu.addEventListener("click", toggleMenu);
+document.getElementById("lay").onclick = function () {
+  this.classList.add("dn");
+  toggleMenu();
+};
